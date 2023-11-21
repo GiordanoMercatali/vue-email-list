38886e2bc2de24efd526a44864ca7d9d53bc4d5e
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-21 16:58:00 +0100

    add: list displayed on screen

--- js/script.js
+++ js/script.js
@@ -3,14 +3,37 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            mailList: [],
             
         };
     },
 
     methods:{
+        getEmails: function(){
+            for(let i = 0; i < 10; i++){
+                axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp)=>{
+                console.log(resp.data.response);
+                this.mailList.push(resp.data.response);
+                console.log(this.mailList);
+            })
+            console.log(this.mailList[i]);
+            }
+            return this.mailList;
+        }
     },
 
     created(){
-        
+        // axios
+        //     .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        //     .then((resp)=>{
+        //         this.mailList[0] = resp.data.response;
+        //         console.log(this.mailList[0]);
+        //     })
+
+        this.getEmails();
     },
+
+
 }).mount("#app");
